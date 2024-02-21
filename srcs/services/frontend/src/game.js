67841bd82042cbec2d464cc8a2e2fca0b8c3a2b6
Maria FLORES VILLEGAS <mflores-@student.42.fr>
2024-02-21 19:47:42 +0100

    feat: added eventListener for resizing window, to be fixed: effin CSS for paddles :'(

--- srcs/services/frontend/src/game.js
+++ srcs/services/frontend/src/game.js
@@ -27,214 +27,6 @@ export const initGameThreeD = () => {
 
     animate();
 };
-///////////////////////////////////////////////////////////////
-
-// class PingPongGame {
-//     constructor() {
-//         this.gameState = 'start';
-//         this.boardCoord = null;
-//         this.paddle1Coord = null;
-//         this.paddle2Coord = null;
-//         this.score1 = null;
-//         this.score2 = null;
-//         this.message = null;
-//         this.score = null;
-//         this.board = null;
-//         this.ball = null;
-//         this.paddle1 = null;
-//         this.paddle2 = null;
-
-//         this.initGame();
-//     }
-
-//     createPaddle(className) {
-//         const paddle = document.createElement('div');
-//         paddle.className = `${className} paddle`;
-//         return paddle;
-//     }
-
-//     createBall(className) {
-//         const ball = document.createElement('div');
-//         ball.className = className;
-
-//         const ballEffect = document.createElement('div');
-//         ballEffect.className = 'ball_effect';
-
-//         ball.appendChild(ballEffect);
-//         return ball;
-//     }
-
-//     createScore(className) {
-//         const box = document.createElement('div');
-//         box.className = 'col-3 text-center align-items-center d-flex flex-column';
-//         const score = document.createElement('h1');
-//         box.appendChild(score);
-//         score.className = `${className} my-3`;
-//         score.textContent = '0';
-//         return box;
-//     }
-
-//     createMessage(className, msg) {
-//         const box = document.createElement('div');
-//         box.className = 'col-5 text-center align-items-center d-flex flex-column';
-//         const message = document.createElement('h1');
-//         box.appendChild(message);
-//         message.className = `${className} my-3 display-7`;
-//         message.textContent = msg;
-//         return box;
-//     }
-
-//     createBoard(className) {
-//         const boardContainer = document.createElement('div');
-//         boardContainer.className = 'container-fluid';
-
-//         const boardRow = document.createElement('div');
-//         boardRow.className = 'row justify-content-center';
-
-//         const board = document.createElement('div');
-//         board.className = `col-12 ${className}`;
-
-//         this.ball = this.createBall('ball');
-//         this.paddle1 = this.createPaddle('paddle_1');
-//         this.paddle2 = this.createPaddle('paddle_2');
-
-//         board.appendChild(this.ball);
-//         board.appendChild(this.paddle1);
-//         board.appendChild(this.paddle2);
-
-//         boardRow.appendChild(board);
-//         boardContainer.appendChild(boardRow);
-
-//         return boardContainer;
-//     }
-
-// 	moveBall = (dx, dy, dxd, dyd, ball_coord, initial_ball_coord, initial_ball) => { 
-// 		if (ball_coord.top <= board_coord.top) { 
-// 			dyd = 1; 
-// 		} 
-// 		if (ball_coord.bottom >= board_coord.bottom) { 
-// 			dyd = 0; 
-// 		} 
-// 		if ( 
-// 			ball_coord.left <= paddle_1_coord.right && 
-// 			ball_coord.top >= paddle_1_coord.top && 
-// 			ball_coord.bottom <= paddle_1_coord.bottom 
-// 		) { 
-// 			dxd = 1; 
-// 			dx = Math.floor(Math.random() * 4) + 3; 
-// 			dy = Math.floor(Math.random() * 4) + 3; 
-// 		} 
-// 		if ( 
-// 			ball_coord.right >= paddle_2_coord.left && 
-// 			ball_coord.top >= paddle_2_coord.top && 
-// 			ball_coord.bottom <= paddle_2_coord.bottom 
-// 		) { 
-// 			dxd = 0; 
-// 			dx = Math.floor(Math.random() * 4) + 3; 
-// 			dy = Math.floor(Math.random() * 4) + 3; 
-// 		} 
-// 		if ( 
-// 			ball_coord.left <= board_coord.left || 
-// 			ball_coord.right >= board_coord.right 
-// 		) { 
-// 			if (ball_coord.left <= board_coord.left) {
-// 				document.querySelector('.player_2_score').textContent = +document.querySelector('.player_2_score').textContent + 1;
-// 			} else { 
-// 				document.querySelector('.player_1_score').textContent = +document.querySelector('.player_1_score').textContent + 1;
-// 			} 
-// 			gameState = 'start'; 
-		  
-// 			ball_coord = initial_ball_coord; 
-// 			ball.style = initial_ball.style;
-// 			document.querySelector('.message').textContent = 'Press Enter'; 
-// 			return;
-// 		} 
-// 		ball.style.top = ball_coord.top + dy * (dyd == 0 ? -1 : 1) + 'px'; 
-// 		ball.style.left = ball_coord.left + dx * (dxd == 0 ? -1 : 1) + 'px'; 
-// 		ball_coord = ball.getBoundingClientRect(); 
-// 		requestAnimationFrame(() => { 
-// 			moveBall(dx, dy, dxd, dyd, ball_coord, initial_ball_coord, initial_ball); 
-// 		}); 
-// 	}
-	
-//     initGame() {
-//         document.getElementById('app').innerHTML = '';
-//         document.getElementById('app').className = 'container';
-
-//         this.score1 = this.createScore('player_1_score');
-//         this.message = this.createMessage('message', 'Press Enter');
-//         this.score2 = this.createScore('player_2_score');
-//         this.score = document.createElement('div');
-//         this.score.className = 'row';
-//         this.score.appendChild(this.score1);
-//         this.score.appendChild(this.message);
-//         this.score.appendChild(this.score2);
-//         document.getElementById('app').appendChild(this.score);
-
-//         this.board = this.createBoard('board');
-//         document.getElementById('app').appendChild(this.board);
-
-// 		// Ball movement
-// 		paddle_1_coord = document.getElementById('app').querySelector('.paddle_1').getBoundingClientRect();
-// 		paddle_2_coord = document.getElementById('app').querySelector('.paddle_2').getBoundingClientRect();
-// 		let paddle_common = document.getElementById('app').querySelector('.paddle').getBoundingClientRect();
-		
-// 		let initial_ball = document.getElementById('app').querySelector('.ball'); 
-// 		let initial_ball_coord = initial_ball.getBoundingClientRect();
-// 		let ball_coord = initial_ball_coord;
-// 		board_coord = document.getElementById('app').querySelector('.board').getBoundingClientRect();
-
-// 		let dx = Math.floor(Math.random() * 4) + 3;
-// 		let dy = Math.floor(Math.random() * 4) + 3;
-// 		let dxd = Math.floor(Math.random() * 2);
-// 		let dyd = Math.floor(Math.random() * 2);
-
-// 		document.addEventListener('keydown', (e) => {
-// 			if (e.key == 'Enter') {
-// 				gameState = (gameState == 'start') ? 'play' : 'start';
-// 				if (gameState == 'play') {
-// 					console.log("enter pressed");
-// 					message.querySelector('h1').innerHTML = 'Game started';
-// 					requestAnimationFrame(() => {
-// 						dx = Math.floor(Math.random() * 4) + 3;
-// 						dy = Math.floor(Math.random() * 4) + 3;
-// 						dxd = Math.floor(Math.random() * 2);
-// 						dyd = Math.floor(Math.random() * 2);
-// 						moveBall(dx, dy, dxd, dyd, ball_coord, initial_ball_coord, initial_ball);
-// 					});
-// 				}
-// 			}
-// 			if (gameState == 'play') {
-// 				if (e.key == 'w') {
-// 					console.log("`w` pressed");
-// 					paddle_1.style.top = Math.max(board_coord.top, paddle_1_coord.top - window.innerHeight * 0.06) + 'px';
-// 					paddle_1_coord = paddle_1.getBoundingClientRect(); 
-// 				}
-// 				if (e.key == 's') {
-// 					console.log("`s` pressed");
-// 					paddle_1.style.top = Math.min(board_coord.bottom - paddle_common.height, paddle_1_coord.top + window.innerHeight * 0.06) + 'px';
-// 					paddle_1_coord = paddle_1.getBoundingClientRect();
-// 				}
-// 				if (e.key == 'ArrowUp') {
-// 					console.log("`arrowUp` pressed");
-// 					paddle_2.style.top = Math.max(board_coord.top, paddle_2_coord.top - window.innerHeight * 0.1) + 'px';
-// 					paddle_2_coord = paddle_2.getBoundingClientRect();
-// 				}
-// 				if (e.key == 'ArrowDown') {
-// 					console.log("`arrowDown` pressed");
-// 					paddle_2.style.top = Math.min(board_coord.bottom - paddle_common.height, paddle_2_coord.top + window.innerHeight * 0.1) + 'px';
-// 					paddle_2_coord = paddle_2.getBoundingClientRect();
-// 				}
-// 			}
-// 		});
-//     }
-// }
-
-// export const initGameTwoD = () => {
-// 	const pingPongGame = new PingPongGame();
-// }
-
-///////////////////////////////////////////////////////////////
 
 // Ping pong elements
 let gameState;
@@ -247,6 +39,10 @@ let message;
 let score;
 let board;
 let ball;
+let initial_ball;
+let initial_ball_coord;
+let ball_coord;
+let paddle_common;
 let paddle_1;
 let paddle_2;
 
@@ -272,18 +68,24 @@ export const initGameTwoD = () => {
 	// Ball movement
 	paddle_1_coord = document.getElementById('app').querySelector('.paddle_1').getBoundingClientRect();
 	paddle_2_coord = document.getElementById('app').querySelector('.paddle_2').getBoundingClientRect();
-    let paddle_common = document.getElementById('app').querySelector('.paddle').getBoundingClientRect();
+    paddle_common = document.getElementById('app').querySelector('.paddle').getBoundingClientRect();
 	
-	let initial_ball = document.getElementById('app').querySelector('.ball'); 
-    let initial_ball_coord = initial_ball.getBoundingClientRect();
-    let ball_coord = initial_ball_coord;
+	initial_ball = document.getElementById('app').querySelector('.ball'); 
+    initial_ball_coord = initial_ball.getBoundingClientRect();
+    ball_coord = initial_ball_coord;
     board_coord = document.getElementById('app').querySelector('.board').getBoundingClientRect();
 
+	// `dx` and `dy` determine the speed of the ball at the beginning
     let dx = Math.floor(Math.random() * 4) + 3;
     let dy = Math.floor(Math.random() * 4) + 3;
+	// `dxd` and `dyd` determine the initial direction of the ball along the x and y axes
     let dxd = Math.floor(Math.random() * 2);
     let dyd = Math.floor(Math.random() * 2);
 
+    // Event listener for window resize
+    window.addEventListener('resize', updatePositionsOnResize);
+
+	// Key events and movements
     document.addEventListener('keydown', (e) => {
         if (e.key == 'Enter') {
             gameState = (gameState == 'start') ? 'play' : 'start';
@@ -291,14 +93,16 @@ export const initGameTwoD = () => {
 				console.log("enter pressed");
 				message.querySelector('h1').innerHTML = 'Game started';
                 requestAnimationFrame(() => {
+					window.addEventListener('resize', updatePositionsOnResize);
                     dx = Math.floor(Math.random() * 4) + 3;
                     dy = Math.floor(Math.random() * 4) + 3;
                     dxd = Math.floor(Math.random() * 2);
                     dyd = Math.floor(Math.random() * 2);
-                    moveBall(dx, dy, dxd, dyd, ball_coord, initial_ball_coord, initial_ball);
+                    moveBall(dx, dy, dxd, dyd);
                 });
             }
         }
+
         if (gameState == 'play') {
             if (e.key == 'w') {
 				console.log("`w` pressed");
@@ -310,16 +114,16 @@ export const initGameTwoD = () => {
 				paddle_1.style.top = Math.min(board_coord.bottom - paddle_common.height, paddle_1_coord.top + window.innerHeight * 0.06) + 'px';
 				paddle_1_coord = paddle_1.getBoundingClientRect();
             }
-			if (e.key == 'ArrowUp') {
-				console.log("`arrowUp` pressed");
-				paddle_2.style.top = Math.max(board_coord.top, paddle_2_coord.top - window.innerHeight * 0.1) + 'px';
-				paddle_2_coord = paddle_2.getBoundingClientRect();
-            }
-            if (e.key == 'ArrowDown') {
-				console.log("`arrowDown` pressed");
-				paddle_2.style.top = Math.min(board_coord.bottom - paddle_common.height, paddle_2_coord.top + window.innerHeight * 0.1) + 'px';
-				paddle_2_coord = paddle_2.getBoundingClientRect();
-            }
+			// if (e.key == 'ArrowUp') {
+			// 	console.log("`ArrowUp` pressed");
+			// 	paddle_2.style.top = Math.max(board_coord.top, paddle_2_coord.top - window.innerHeight * 0.1) + 'px';
+			// 	paddle_2_coord = paddle_2.getBoundingClientRect();
+            // }
+            // if (e.key == 'ArrowDown') {
+			// 	console.log("`ArrowDown` pressed");
+			// 	paddle_2.style.top = Math.min(board_coord.bottom - paddle_common.height, paddle_2_coord.top + window.innerHeight * 0.1) + 'px';
+			// 	paddle_2_coord = paddle_2.getBoundingClientRect();
+            // }
         }
     });
 };
@@ -343,53 +147,53 @@ const createBall = ( className ) => {
     return (ball);
 };
 
-const moveBall = (dx, dy, dxd, dyd, ball_coord, initial_ball_coord, initial_ball) => { 
-	if (ball_coord.top <= board_coord.top) { 
-		dyd = 1; 
-	} 
-	if (ball_coord.bottom >= board_coord.bottom) { 
-		dyd = 0; 
-	} 
-	if ( 
-		ball_coord.left <= paddle_1_coord.right && 
-		ball_coord.top >= paddle_1_coord.top && 
-		ball_coord.bottom <= paddle_1_coord.bottom 
-	) { 
-		dxd = 1; 
-		dx = Math.floor(Math.random() * 4) + 3; 
-		dy = Math.floor(Math.random() * 4) + 3; 
-	} 
-	if ( 
-		ball_coord.right >= paddle_2_coord.left && 
-		ball_coord.top >= paddle_2_coord.top && 
-		ball_coord.bottom <= paddle_2_coord.bottom 
-	) { 
-		dxd = 0; 
-		dx = Math.floor(Math.random() * 4) + 3; 
-		dy = Math.floor(Math.random() * 4) + 3; 
-	} 
-	if ( 
-		ball_coord.left <= board_coord.left || 
-		ball_coord.right >= board_coord.right 
-	) { 
+const moveBall = (dx, dy, dxd, dyd) => {
+	if (ball_coord.top <= board_coord.top) {
+		dyd = 1;
+	}
+	if (ball_coord.bottom >= board_coord.bottom) {
+		dyd = 0;
+	}
+	if (
+		ball_coord.left <= paddle_1_coord.right &&
+		ball_coord.top >= paddle_1_coord.top &&
+		ball_coord.bottom <= paddle_1_coord.bottom
+	) {
+		dxd = 1;
+		dx = Math.floor(Math.random() * 4) + 3;
+		dy = Math.floor(Math.random() * 4) + 3;
+	}
+	if (
+		ball_coord.right >= paddle_2_coord.left &&
+		ball_coord.top >= paddle_2_coord.top &&
+		ball_coord.bottom <= paddle_2_coord.bottom
+	) {
+		dxd = 0;
+		dx = Math.floor(Math.random() * 4) + 3;
+		dy = Math.floor(Math.random() * 4) + 3;
+	}
+	if (
+		ball_coord.left <= board_coord.left ||
+		ball_coord.right >= board_coord.right
+	) {
 		if (ball_coord.left <= board_coord.left) {
 			document.querySelector('.player_2_score').textContent = +document.querySelector('.player_2_score').textContent + 1;
-		} else { 
+		} else {
 			document.querySelector('.player_1_score').textContent = +document.querySelector('.player_1_score').textContent + 1;
-		} 
-		gameState = 'start'; 
-	  
-		ball_coord = initial_ball_coord; 
+		}
+		gameState = 'start';
+
+		ball_coord = initial_ball_coord;
 		ball.style = initial_ball.style;
-		document.querySelector('.message').textContent = 'Press Enter'; 
+		document.querySelector('.message').textContent = 'Press Enter';
 		return;
-	} 
-	ball.style.top = ball_coord.top + dy * (dyd == 0 ? -1 : 1) + 'px'; 
-	ball.style.left = ball_coord.left + dx * (dxd == 0 ? -1 : 1) + 'px'; 
-	ball_coord = ball.getBoundingClientRect(); 
-	requestAnimationFrame(() => { 
-		moveBall(dx, dy, dxd, dyd, ball_coord, initial_ball_coord, initial_ball); 
-	}); 
+	}
+	ball.style.top = ball_coord.top + dy * (dyd == 0 ? -1 : 1) + 'px';
+	ball.style.left = ball_coord.left + dx * (dxd == 0 ? -1 : 1) + 'px';
+	ball_coord = ball.getBoundingClientRect();
+	requestAnimationFrame(() => {
+		moveBall(dx, dy, dxd, dyd); 
+	});
 }
 
 // Score
@@ -406,7 +210,7 @@ const createScore = ( className ) => {
 // Message
 const createMessage = ( className, msg ) => {
 	const box = document.createElement('div');
-	box.className = 'col-5 text-center align-items-center d-flex flex-column';
+	box.className = 'col-6 text-center align-items-center d-flex flex-column';
     const message = document.createElement('h1');
 	box.appendChild(message);
     message.className = `${className} my-3 display-7`;
@@ -425,16 +229,31 @@ const createBoard = (className) => {
     const board = document.createElement('div');
     board.className = `col ${className}`;
 
+    const dottedLine = document.createElement('div');
+    dottedLine.className = 'dotted-line';
+
     ball = createBall('ball');
     paddle_1 = createPaddle('paddle_1');
     paddle_2 = createPaddle('paddle_2');
-
+	
     board.appendChild(ball);
     board.appendChild(paddle_1);
     board.appendChild(paddle_2);
+	board.appendChild(dottedLine);
 
     boardRow.appendChild(board);
     boardContainer.appendChild(boardRow);
 
     return (boardContainer);
 };
+
+const updatePositionsOnResize = () => {
+	console.log("window resize detected, updating ping pong elements");
+	paddle_1_coord = document.getElementById('app').querySelector('.paddle_1').getBoundingClientRect();
+	paddle_2_coord = document.getElementById('app').querySelector('.paddle_2').getBoundingClientRect();
+	paddle_common = document.getElementById('app').querySelector('.paddle').getBoundingClientRect();
+	board_coord = document.getElementById('app').querySelector('.board').getBoundingClientRect();
+	initial_ball = document.getElementById('app').querySelector('.ball'); 
+    initial_ball_coord = initial_ball.getBoundingClientRect();
+    ball_coord = initial_ball_coord;
+}
