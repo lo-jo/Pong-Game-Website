ccd168533ac74c17e09cfdb631bb486c888f3d48
lorraine <lorraine.jospin@gmail.com>
2024-02-22 12:24:08 +0100

    dev tmp for redis

--- srcs/services/backend/src/trs/trs/settings.py
+++ srcs/services/backend/src/trs/trs/settings.py
@@ -87,6 +87,14 @@ TEMPLATES = [
 # Daphne
 ASGI_APPLICATION = "trs.asgi.application"
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
