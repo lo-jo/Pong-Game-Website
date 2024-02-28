8d02c35829ed903d9e5d2be78e3f520afeedf812
lorraine <lorraine.jospin@gmail.com>
2024-02-28 13:01:54 +0100

    [UPDATE] class user on profile.js

--- srcs/services/frontend/src/profile.js
+++ srcs/services/frontend/src/profile.js
@@ -1,6 +1,49 @@
+class User{
+    constructor(username, pic, id, email, bio, picpath) {
+        this.username = username;
+        this.pic = pic;
+        this.id = id;
+        this.email = email;
+        this.bio = bio;
+        this.picpath = picpath;
+      }
+      getProfilePicPath() {
+        return "http://localhost:8000" + this.pic;
+    }
+      getFriendReq() {
+        return "http://localhost:8000/users/friendship/" + this.username + "/";
+    }
+}
+
+const addFriend = (user) => {
+    const jwtAccess = localStorage.getItem('token');
+    fetch(user.getFriendReq(), {
+        method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${jwtAccess}`,
+			'Content-Type': 'application/json',
+		},
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Handle successful login, e.g., store token in local storage
+        console.log('Succesfully signed up', data);
+        console.log("data: ", data);
+        alert("SUCCESFULLY ADDED AS FRIEND")
+    })
+    .catch(error => {
+        console.error('Impossible friendship : ', error);
+        alert("ERRRRRRROR");
+    });
+}
+
 function display_profile() {
 	const jwtAccess = localStorage.getItem('token');
-    console.log("WHY ARE YOU NOT EXECUTING THIS PROPERLY");
 
 	fetch('http://localhost:8000/users/profile/', {
 		method: 'GET',
@@ -11,7 +54,6 @@ function display_profile() {
 	})
 	.then(response => {
         if (!response.ok) {
-            // Check for unauthorized (status code 401) or other errors
             if (response.status === 401) {
                 console.error('Unauthorized access. Please log in.');
 				document.getElementById('content').innerHTML = "You need to login to see this page."
@@ -23,23 +65,26 @@ function display_profile() {
         return response.json();
     })
 	.then(data => {
-	  const username = data.username;
-      console.log(username);
-	  const pic = data.profile_pic;
-      console.log(pic);
-	  const nb = data.id;
-	  const email = data.email;
-	  const bio = data.bio;
-	  const picpath = "http://localhost:8000" + pic;
-	  console.log(picpath);
+        const currentUser = new User(data.username, data.profile_pic, data.id, data.email, data.bio)
+        
+        const friendRequestLink = document.createElement('a');
+        friendRequestLink.href = '#';  // Set the href as needed
+        friendRequestLink.innerText = 'Send Friend Request';
+        friendRequestLink.addEventListener('click', (event) => {
+            event.preventDefault();
+            addFriend(currentUser); // Pass user ID or any necessary data to the function
+        });
+
+        // Append the link to the 'friendRequest' div
+        document.getElementById('friendRequest').appendChild(friendRequestLink);
 	 
 
 	  // Display attributes
-        document.getElementById('username').innerText = username;
-        document.getElementById("pic").src = picpath;
-        document.getElementById('email').innerText = email;
-        document.getElementById('bio').innerText = bio;
-        document.getElementById('nb').innerText = nb;
+        document.getElementById('username').innerText = currentUser.username;
+        document.getElementById("pic").src = currentUser.getProfilePicPath();
+        document.getElementById('email').innerText = currentUser.email;
+        document.getElementById('bio').innerText = currentUser.bio;
+        document.getElementById('nb').innerText = currentUser.id;
 	})
 	.catch(error => console.error('Error:', error));
   
@@ -57,7 +102,7 @@ export const profileUser = () => {
 		<div class="row" id="nb"></div>
 		<div class="row" id="email"></div>
 		<div class="row" id="bio"></div>
-        <div class="row" id="add as friend">ADD AS FRIEND</div>
+        <div class="row" id="friendRequest"></div>
         <div class="row" id="friendlist"> DISPLAY FRIEND LIST</div>
         <div class="row" id="matchHistory">MATCH HISTORY</div>
         <div class="row" id="matchHistory">STATS (wins, losses)</div>
